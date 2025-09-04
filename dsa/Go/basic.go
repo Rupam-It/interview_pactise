57b944fd59b5aff3dda4987914d6b5ed50a23fef
Rupam-It <mannarupam3@gmail.com>
2025-09-04 11:58:39 +0530

    sept 4
    Signed-off-by: Rupam-It <mannarupam3@gmail.com>

---
+++ dsa/Go/basic.go
@@ -0,0 +1,39 @@
+// package main
+
+// import "fmt"
+
+// type Person struct {
+// 	name    string
+// 	age     int
+// 	collage string
+// }
+
+// func (p Person) show() {
+// 	fmt.Printf("Hello, my name is %s, I'm %d years old, and I study at %s.\n", p.name, p.age, p.collage)
+// }
+
+// // func main() {
+// // 	fmt.Println("hello world ")
+// // 	// var age = 1
+// // 	// if age >= 18 {
+// // 	// 	fmt.Print("can vote")
+// // 	// } else {
+// // 	// 	fmt.Print("not allow to vote")
+// // 	// }
+// // 	// i := 0
+// // 	// for i < 10 {
+// // 	// 	println(i)
+// // 	// 	i++
+// // 	// }
+
+// // 	// // var slice []int
+// // 	// slice := make([]int, 5)
+// // 	// fmt.Println(slice)
+// // 	p1 := Person{name: "Rupam", age: 22, collage: "rcciit"}
+// // 	p1.show()
+
+// // 	var p *int
+// // 	i := 21
+// // 	p = &i
+// // 	fmt.Print(*p)
+// // }
---
+++ dsa/Go/gorutine.go
@@ -0,0 +1,64 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"sync"
+// )
+
+// func worker(id int, wg *sync.WaitGroup) {
+// 	defer wg.Done()
+// 	fmt.Printf("worker start %d\n", id)
+// 	fmt.Printf("worker finish %d\n", id)
+// }
+
+// func main() {
+
+// 	// ch := make(chan int, 2)
+
+// 	// go func() {
+// 	// 	ch <- 40
+// 	// 	ch <- 10
+// 	// }()
+
+// 	// val := <-ch
+// 	// val2 := <-ch
+// 	// fmt.Println("Received:", val, val2)
+// 	var wg sync.WaitGroup
+
+// 	for i := 1; i < 4; i++ {
+// 		wg.Add(1)
+// 		go worker(i, &wg)
+// 	}
+
+// 	wg.Wait()
+// 	fmt.Println("All workers finished")
+// }
+
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var counter int
+var mu sync.Mutex
+
+func increment(wg *sync.WaitGroup) {
+	defer wg.Done()
+	mu.Lock()
+	counter++
+	mu.Unlock()
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go increment(&wg)
+	}
+	wg.Wait()
+	fmt.Println("Final Counter:", counter)
+
+}
